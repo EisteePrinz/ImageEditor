ec81c7c28ea1ff272d4565dfd7fb0933a512c5bf
Murat Yilmaz <murat.yil90@gmail.com>
2016-10-25 18:39:26 +0200

    Added EditorPanel Class

--- ImageEditorFrame.java
+++ ImageEditorFrame.java
@@ -10,9 +10,22 @@ import javax.imageio.*;
 public class ImageEditorFrame extends JFrame {
 	public ImageEditorFrame(String title){
 		super(title);
+		
+		panel = new ImageEditorPanel();
+		add.panel();
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(400,300);
 		setVisible(true);
+		
+		
+		private void setDummyImage(){
+			BufferedImage bufferedImage =
+					new BufferedImage(400,300, BufferedImage.TYPE-INT-RGB);
+			Graphics g = bufferedImage.getGraphics();
+			get.setColor(Color.YELLOW);
+			g.fillOval(10,10,380,280);
+			panel.setImage(bufferedImage);
+		}
 	}
 	
 	
